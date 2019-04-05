455740a7b95265a71bec8c6482a3e959c1e936fc
Jeff Rutland <jeff.rutland@gmail.com>
2019-04-05 15:19:43 -0500

    Update iis-nick.ps1

--- iis-nick.ps1
+++ iis-nick.ps1
@@ -4,4 +4,4 @@ dism /online /enable-feature /featurename:IIS-WebServerRole
 # Set the home page.
 Set-Content `
   -Path "C:\\inetpub\\wwwroot\\Default.htm" `
-  -Value "<html><body><h2>Whassup, ShaNickNick!?</h2></body></html>"
+  -Value "<html><body><h2>SUP, ShaNickNick!?  It's me, Azure.  Remember BARRS LEWIS!?</h2></body></html>"
