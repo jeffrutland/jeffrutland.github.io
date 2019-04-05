7afc093d436c5666d0349f80d53fa2714153f9f7
Jeff Rutland <jeff.rutland@gmail.com>
2019-04-05 15:25:35 -0500

    Delete iis-nick.ps1

--- iis-nick.ps1
+++
@@ -1,7 +0,0 @@
-# Install IIS.
-dism /online /enable-feature /featurename:IIS-WebServerRole
-
-# Set the home page.
-Set-Content `
-  -Path "C:\\inetpub\\wwwroot\\Default.htm" `
-  -Value "<html><body><h2>SUP, ShaNickNick!?  It's me, Azure.  Remember BARRS LEWIS!?</h2></body></html>"
